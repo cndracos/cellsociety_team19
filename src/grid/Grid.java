a919b94a859befd99e8b13e40dc23bbd7d6397de
Charles <cnd11@duke.edu>
2018-02-02 17:14:18 -0500

    Added Grid interface and FireGrid subclass

---
+++ src/grid/Grid.java
@@ -0,0 +1,24 @@
+package grid;
+
+import cell.Cell;
+import java.util.ArrayList;
+
+public abstract class Grid {
+	private Cell[][] population;
+	
+	public Grid (int n, int k) {
+		population = new Cell[n][k];
+	}
+	
+	public Cell get(int n, int k) {
+		return population[n][k];
+	}
+	
+	public void add (Cell c, int n, int k) {
+		population[n][k] = c;
+	}
+	
+	public abstract ArrayList<Cell> getNeighbors(int n, int k);
+	
+	public abstract void addNeighbors (int n, int k);
+}
