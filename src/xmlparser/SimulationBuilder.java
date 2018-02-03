0a913337dba97f2b86e22d77a72d1cb52c7b352c
Dylan Powers <dylan.powers@duke.edu>
2018-02-02 22:10:27 -0500

    Merged CellSociety to cooperate with new FireGrid subclass

--- src/xmlparser/SimulationBuilder.java
+++ src/xmlparser/SimulationBuilder.java
@@ -3,6 +3,8 @@ package xmlparser;
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -10,38 +12,61 @@ import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
+import grid.*;
+
 public class SimulationBuilder {
 	
 	private static final String ACCEPTABLE_EXTENSION = "*.xml";
-	private final FileChooser FILE_CHOOSER = buildChooser(ACCEPTABLE_EXTENSION);
-	// fields of the XML file that the class XMLFile will need to access
+	private static FileChooser FILE_CHOOSER;
+	// the specific file that builds this simulation
+	private static File XMLFile;
+	// fields of the XML file that the class XMLParser will need to access
 	public static final List<String> DATA_FIELDS = Arrays.asList(new String[] {
 			"model",
 			"rows", 
 			"columns", 
-			"probability"
+			"probability",
 	});
+	public static final String INITIAL_DATA_FIELD = new String("initialStates");
 	
 	public SimulationBuilder(Stage stage) {
-		File XMLFile = FILE_CHOOSER.showOpenDialog(stage);
+		FILE_CHOOSER = buildChooser(ACCEPTABLE_EXTENSION);
+		XMLFile = FILE_CHOOSER.showOpenDialog(stage);
+	}
+	
+	// build the grid
+	public Grid build() {
 		if (XMLFile != null) {
 			try {
-				System.out.println(new XMLParser("simulation"));
+				XMLParser parser = new XMLParser("simulation");
+				Map<String, String> gridProperties = parser.getGridProperties(XMLFile);
+				// extract all of the information that we need to form the grid
+				String simulationType = gridProperties.get("model").toString();
+				int rows = Integer.parseInt(gridProperties.get("rows").toString());
+				int cols = Integer.parseInt(gridProperties.get("columns").toString());
+				double probability = Double.parseDouble(gridProperties.get("probability").toString());
+				// extract the percentages for the initial states of the cells
+				Map<String, double[]> initialStates = parser.getInitialStates(XMLFile);
+				return generateGrid(simulationType, rows, cols, probability, initialStates);
 			} catch (XMLException e) {
 				Alert alert = new Alert(AlertType.ERROR);
 				alert.setContentText(e.getMessage());
 				alert.showAndWait();
 			}
 		}
-		
-		
-		
-		
 	}
 	
+	// select which type of grid we want to initialize based upon the simulation type
+	public Grid generateGrid(String simulationType, int rows, int cols, double probability, Map<String, double[]> initialStates) {
+		switch (simulationType) {
+			case "Fire": 
+				return new FireGrid(rows, cols, probability, initialStates);
+		}
+			
+			
+	}
 	
-	
-	
+	// make the file chooser that will be displayed to the user
 	private FileChooser buildChooser(String acceptableExtension) {
 		FileChooser chooser = new FileChooser();
 		chooser.setTitle("Choose a simulation setup file");
