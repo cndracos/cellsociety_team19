d2683551189313c8d5e3a30e4bad484cf8b13246
Charles <cnd11@duke.edu>
2018-02-02 22:44:25 -0500

    updated all grids to deal with key map

--- src/grid/LifeGrid.java
+++ src/grid/LifeGrid.java
@@ -1,18 +1,25 @@
 package grid;
 
 import cell.Cell;
+import cell.LifeCell;
+
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Random;
 
 public class LifeGrid extends Grid {
 	private ArrayList<Cell>[][] neighbors;
 	private int n;
 	private int k;
+	private Random rand;
 
-	public LifeGrid(int n, int k) {
-		super(n, k);
+	public LifeGrid(int n, int k, Map<String, double[]> keys) {
+		super(n, k, keys);
 		neighbors = new ArrayList[n][k];
 		this.n = n;
 		this.k = k;
+		rand = new Random();
+		init();
 	}
 	
 	public ArrayList<Cell> getNeighbors(int n, int k){
@@ -43,4 +50,19 @@ public class LifeGrid extends Grid {
 			}
 		}
 	}
+	
+	public void init() {
+		double[] probLife = this.getKeys().get("ALIVE");
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < k; j++) {
+					double randD = rand.nextDouble();
+					if (randD > probLife[0] && randD <= probLife[1]) 
+						this.add(new LifeCell("ALIVE", 0), i, j);
+					else this.add(new LifeCell("DEAD", 0), i, j);
+				}
+				//root.getChildren().add(this.get(i, j));
+			}
+		this.addNeighbors();
+	}
 }
+
