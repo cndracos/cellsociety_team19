cdf6731acda16f1b07ee11fc7d3a8a81c4438757
yl366 <yl366@duke.edu>
2018-02-04 16:48:26 -0500

    commit reproduction

--- src/cell/WatorCell.java
+++ src/cell/WatorCell.java
@@ -11,6 +11,8 @@ public class WatorCell extends Cell{
 	private double sharkR;
 	private double sharkE;
 	private Random rand;
+	private int fishMoves;
+	private int sharkMoves;
 	private final int fishE = 7;
 	
 	public WatorCell(String currState,double fishR,double sharkR,double sharkE){
@@ -18,6 +20,8 @@ public class WatorCell extends Cell{
 		this.fishR = fishR;
 		this.sharkR = sharkR;
 		this.sharkE = sharkE;
+		int fishMoves = 0;
+		int sharkMoves = 0;
 		rand = new Random();
 	}
 	
@@ -37,38 +41,64 @@ public class WatorCell extends Cell{
 	 */
 	protected String updateByRule() {
 		if(getState().equals("FISH")){
-			//System.out.print("enter fish");
 			ArrayList<WatorCell> waters = getTypes("WATER");
+			WatorCell nextCell = null;
 			//if there is an unoccupied free cell, move to it
 			if(waters.size() > 0){
+				//System.out.println("fish moves");
 				int key = rand.nextInt(waters.size());
+				nextCell = waters.get(key);
 				this.newState ="WATER";
-				waters.get(key).newState = "FISH";
+				nextCell.newState = "FISH";
+				fishMoves += 1;
+				//reproduce fish
+				if(fishMoves == fishR){
+					//System.out.println("fish reproduces");
+					this.newState = "FISH";
+					fishMoves = 0;
+				}
 			}
 		}
 		
 		else if(getState().equals("SHARK")){
-			//System.out.print("enter shark");
 			ArrayList<WatorCell> fishs = getTypes("FISH");
 			ArrayList<WatorCell> waters = getTypes("WATER");
-			//if there is a fish, devour it
-			if(fishs.size() > 0){
-				int key = rand.nextInt(fishs.size());
-				this.newState ="WATER";
-				fishs.get(key).newState = "SHARK";
-				sharkE += fishE;
+			WatorCell nextCell = null;
+			//if it is eligible to move
+			if(fishs.size() > 0 || waters.size() > 0){
+				//if there is a fish, devour it
+				if(fishs.size() > 0){
+					System.out.println("shark devour fish");
+					int key = rand.nextInt(fishs.size());
+					nextCell = fishs.get(key);
+					this.newState ="WATER";
+					nextCell.newState = "SHARK";
+					sharkE += fishE;
+					
+				}
+				//if there is an unoccupied free cell, move to it
+				else if(waters.size() > 0){
+					System.out.println("shark moves");
+					int key = rand.nextInt(waters.size());
+					nextCell = waters.get(key);
+					this.newState = "WATER";
+					nextCell.newState = "SHARK";
+				}
+				
 				sharkE -= 1;
-			}
-			//if there is an unoccupied free cell, move to it
-			else if(waters.size() > 0){
-				int key = rand.nextInt(waters.size());
-				this.newState = "WATER";
-				waters.get(key).newState = "SHARK";
-				sharkE -= 1;
-			}
-			//if a shark consumed all energy, it dies
-			if(sharkE == 0){
-				this.newState = "WATER";
+				sharkMoves += 1;
+				
+				//if a shark consumed all energy, it dies
+				if(sharkE == 0){
+					System.out.println("shark dies");
+					this.newState = "WATER";
+				}
+				//reproduce shark
+				else if(sharkMoves == sharkR && nextCell != null){
+					System.out.println("shark reproduces");
+					this.newState = "SHARK";
+					sharkMoves = 0;
+				}
 			}
 		}
 		
