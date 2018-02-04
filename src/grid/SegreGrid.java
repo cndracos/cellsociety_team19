d7825985ebb6128b1ff99acbcdbe4dfbc5dd5e6f
Charles <cnd11@duke.edu>
2018-02-04 12:13:20 -0500

    undid segregrid conflict

--- src/grid/SegreGrid.java
+++ src/grid/SegreGrid.java
@@ -18,37 +18,6 @@ public class SegreGrid extends Grid {
 		rand = new Random();
 		init();
 	}
-	
-
-	public ArrayList<Cell> getNeighbors(int n, int k){
-		return neighbors[n][k];
-	}
-	
-	public void addNeighbors() {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < k; j++) {
-				neighbors[i][j] = new ArrayList<Cell>();
-				if (i-1>=0) {
-					neighbors[i][j].add(this.get(i-1, j));
-					if (j-1>=0) {
-						neighbors[i][j].add(this.get(i-1, j-1));
-						neighbors[i][j].add(this.get(i, j-1));
-					}
-					if (j+1<k) {
-						neighbors[i][j].add(this.get(i-1, j+1));
-						neighbors[i][j].add(this.get(i, j+1));
-					}
-				}
-				if (i+1<n) {
-					neighbors[i][j].add(this.get(i+1, j));
-					if (j-1>=0) neighbors[i][j].add(this.get(i+1, j-1));
-					if (j+1<k) neighbors[i][j].add(this.get(i+1, j+1));
-				}
-				this.get(i, j).setNeighbors(neighbors[i][j]);
-			}
-		}
-	}
-	
     
 	public void init() {
 		/**
@@ -86,9 +55,6 @@ public class SegreGrid extends Grid {
 				}
 			}
 		}
-
-		Collections.shuffle(disatisfied);
-
 		
 		Collections.shuffle(empty);
 		Collections.shuffle(disatisfied);
