68ef7daacfadc2a86fc3d5d5773c8d7b0c2a7b96
Dylan Powers <dylan.powers@duke.edu>
2018-02-08 16:37:57 -0500

    Update WatorGrid.java

--- src/grid/WatorGrid.java
+++ src/grid/WatorGrid.java
@@ -46,6 +46,7 @@ public class WatorGrid extends Grid {
 	 * corresponding edge cell on the other side of the screen to create the 'taurus'
 	 * that represents the Wa-Tor simulation world
 	 */
+	@Override
 	public void addNeighbors() {
 			for (int i = 0; i < this.getRows(); i++) {
 				for (int j = 0; j < this.getCols(); j++) {
