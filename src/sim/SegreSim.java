60a9e25c352ca76238db4ecb521aeb89f72831d8
Charles <cnd11@duke.edu>
2018-02-09 21:00:22 -0500

    Split grid into two packages

--- src/sim/SegreSim.java
+++ src/sim/SegreSim.java
@@ -1,69 +1,62 @@
-package grid;
-
-import cell.Cell;
-import cell.SegreCell;
+package sim;
 
 import java.util.ArrayList;
+
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
-public class SegreGrid extends Grid {
-	private double satisfied;
-	private Random rand;
+import cell.SegreCell;
+import grid.Grid;
 
-	public SegreGrid(int n, int k, int length, int width, double satisfied, HashMap<String, double[]> keys) {
-		super(n, k, length, width, keys);
+public class SegreSim extends Sim{
+	private double satisfied;
+	
+	public SegreSim(int n, int k, int length, int width, 
+			double satisfied, HashMap<String, double[]> keys, String grid) {
+		super(n, k, length, width, keys, grid);
 		this.satisfied = satisfied;
-		rand = new Random();
-		init();
 	}
-	/**
-	 * Initializes grid by adding cells to indexes, the calls adds the cell to
-	 * its surrounding neighbor ArrayLists, then sets neighbors when all
-	 * cells are added
-	 */
+
+	@Override
 	public void init() {
 		double[] probX = this.getKeys().get("X");
 		double[] probO = this.getKeys().get("O");
-		for (int i = 0; i < this.getRows(); i++) {
-			for (int j = 0; j < this.getCols(); j++) {
+		Grid sgrid = this.getGrid();
+		Random rand = this.getRand();
+		for (int i = 0; i < sgrid.getRows(); i++) {
+			for (int j = 0; j < sgrid.getCols(); j++) {
 				//makes an arbitrary SegreCell
 				SegreCell s;
 				double randD = rand.nextDouble();
 				if (randD >= probX[0] && randD < probX[1]) {
 					s = new SegreCell("X", satisfied);
-					this.add(s, i, j);
+					sgrid.add(s, i, j);
 				}
 				else if (randD >= probO[0] && randD < probO[1]) {
 					s = new SegreCell("O", satisfied);
-					this.add(s, i, j);
+					sgrid.add(s, i, j);
 				}
 				else {
 					s = new SegreCell("EMPTY", satisfied);
-					this.add(s, i, j);
+					sgrid.add(s, i, j);
 				}
-				this.updateNeighbors(i, j, s, "Segre");
+				sgrid.updateNeighbors(i, j, s, "Segre");
 			}
 		}
-		this.setNeighbors();
-    }
-	/**
-	 * Updates the cells on screen, but only looks for disatisfied cells and empty cells, 
-	 * then shuffles them for randomness, then runs a loop of the smaller amount of
-	 * the two types of cells, and swaps them. 
-	 * Then finally sets the new states of the cells
-	 */
+		sgrid.setNeighbors();	
+	}
+	
 	@Override
 	public void update() {
 		//creates two ArrayList<SegreCell> to store empty and disatisfied cells
 		ArrayList<SegreCell> empty = new ArrayList<SegreCell>();
 		ArrayList<SegreCell> disatisfied = new ArrayList<SegreCell>();
-		for (int i = 0; i < this.getRows(); i++) {
-			for (int j = 0; j < this.getCols(); j++) {
+		Grid sgrid = this.getGrid();
+		for (int i = 0; i < sgrid.getRows(); i++) {
+			for (int j = 0; j < sgrid.getCols(); j++) {
 				//store the current SegreCell s
-				SegreCell s = (SegreCell) this.get(i, j);
+				SegreCell s = (SegreCell) sgrid.get(i, j);
 				if (s.getState().equals("EMPTY")) {
 					empty.add(s);
 				}
@@ -72,21 +65,21 @@ public class SegreGrid extends Grid {
 				}
 			}
 		}
-		
+				
 		//shuffles the two lists to make the swapping random (and more efficient)
 		Collections.shuffle(empty);
 		Collections.shuffle(disatisfied);
-		
+			
 		for (int l = 0; l < Math.min(empty.size(), disatisfied.size()); l++) {
 			//swaps the two states, need to getState() disatisfied because we do not know
 			//if it is an "X" or an "O"
 			empty.get(l).changeState2(disatisfied.get(l).getState());
 			disatisfied.get(l).changeState2("EMPTY");
 		}
-		
-		for (int i = 0; i < this.getRows(); i++) {
-			for (int j = 0; j < this.getCols(); j++) {
-				this.get(i, j).setState();
+				
+		for (int i = 0; i < sgrid.getRows(); i++) {
+			for (int j = 0; j < sgrid.getCols(); j++) {
+				sgrid.get(i, j).setState();
 			}
 		}
 	}
--- src/sim/WatorSim.java
+++ src/sim/WatorSim.java
@@ -1,27 +1,21 @@
-package grid;
+package sim;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
-import cell.*;
+import cell.WatorCell;
+import grid.Grid;
 
-public class WatorGrid extends Grid {
-	private Random rand;
+public class WatorSim extends Sim{
 	private double[] vals;
-
-	public WatorGrid(int n, int k, int length, int width, double[] vals, HashMap<String, double[]> keys) {
-		super(n, k, length, width, keys);
+	
+	public WatorSim(int n, int k, int length, int width, 
+			double[] vals, HashMap<String, double[]> keys, String grid) {
+		super(n, k, length, width, keys, grid);
 		this.vals = vals;
-		rand = new Random();
-		init();
 	}
-	/**
-	 * Initializes the grid by adding the cells to the grid, then calls add 
-	 * cell to ArrayList<Cell> neighbors around it, then sets neighbors when all
-	 * cells are added
-	 */
+
+	@Override
 	public void init() {
 		//gets the array of probs from the key map
 		double[] probF = this.getKeys().get("FISH");
@@ -30,26 +24,28 @@ public class WatorGrid extends Grid {
 		double fishR = vals[0];
 		double sharkR = vals[1];
 		double sharkE = vals[2];
-		for (int i = 0; i < this.getRows(); i++) {
-			for (int j = 0; j < this.getCols(); j++) {
+		Grid wgrid = this.getGrid();
+		Random rand = this.getRand();
+		for (int i = 0; i < wgrid.getRows(); i++) {
+			for (int j = 0; j < wgrid.getCols(); j++) {
 				//creates arbitrary WatorCell to be added
 				WatorCell w;
 				double randD = rand.nextDouble();
 				if (randD >= probF[0] && randD < probF[1]) {
 					w = new WatorCell("FISH", fishR, sharkR, sharkE);
-					this.add(w, i, j); 
+					wgrid.add(w, i, j); 
 				}
 				else if (randD >= probS[0] && randD < probS[1]) {
 					w = new WatorCell("SHARK", fishR, sharkR, sharkE);
-					this.add(w, i, j); 
+					wgrid.add(w, i, j); 
 				}
 				else {
 					w = new WatorCell("WATER", fishR, sharkR, sharkE);
-					this.add(w, i, j);
+					wgrid.add(w, i, j);
 				}
-				this.updateNeighbors(i, j, w, "Wator");
+				wgrid.updateNeighbors(i, j, w, "Wator");
 			}
 		}
-		this.setNeighbors();
-	}
+		wgrid.setNeighbors();
+	}	
 }
